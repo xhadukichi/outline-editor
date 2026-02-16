0692663845b33543c78e78b8cfa50873e28fae8a
xhadukichi <x.haduki@gmail.com>
2026-02-17 07:30:40 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("editor-cache").then(cache => {
+      return cache.addAll([
+        "./0_OutlineEditor.html",
+        "./manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
